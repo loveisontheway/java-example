7a7d1b9be778e9b439127ce058ef8adc95a45980
jiangjialiang <jl.jiang@tytyb.com>
2020-12-07 08:37:28 +0800

    no message

---
+++ src/main/java/com/muxi/java/example/redis/RedisDistributedLock.java
@@ -0,0 +1,55 @@
+package com.muxi.java.example.redis;
+
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Collections;
+
+/**
+ * Redis 分布式锁
+ *
+ * @author jl.jiang 2020/12/4
+ */
+public class RedisDistributedLock {
+
+    private static final String LOCK_SUCCESS = "OK";
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+    private static final Long RELEASE_SUCCESS = 1L;
+
+    /**
+     * 尝试获取分布式锁
+     *
+     * @param jedis      Redis客户端
+     * @param lockKey    锁
+     * @param requestId  请求标识
+     * @param expireTime 超期时间
+     * @return 是否获取成功
+     */
+    public static boolean tryGetDistributedLock(Jedis jedis, String lockKey, String requestId, int expireTime) {
+        // redis version 3.3.0 -> 2.9.0
+        String result = jedis.set(lockKey, requestId, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
+        if (LOCK_SUCCESS.equals(result)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 释放分布式锁
+     *
+     * @param jedis     Redis客户端
+     * @param lockKey   锁
+     * @param requestId 请求标识
+     * @return 是否释放成功
+     */
+    public static boolean releaseDistributedLock(Jedis jedis, String lockKey, String requestId) {
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result = jedis.eval(script, Collections.singletonList(lockKey), Collections.singletonList(requestId));
+        if (RELEASE_SUCCESS.equals(result)) {
+            return true;
+        }
+        return false;
+    }
+}
