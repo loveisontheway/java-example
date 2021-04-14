cf57e125203ce6414233eb45d3894fdf2c978718
jiangjialiang <jl.jiang@tytyb.com>
2021-04-14 17:07:37 +0800

    no message

--- src/main/java/com/muxi/java/example/redisson/RedisController.java
+++ src/main/java/com/muxi/java/example/redisson/RedisController.java
@@ -1,16 +1,13 @@
-package com.muxi.redisson;
+package com.muxi.java.example.redisson;
 
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Redis分布式锁 - redisson实现
