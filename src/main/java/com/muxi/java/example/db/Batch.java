3acbb0e3c231562e0514a0cfcaa811fb71e55ff0
jiangjialiang <jiangjialiang0746@163.com>
2022-05-16 19:36:42 +0800

    no message

---
+++ src/main/java/com/muxi/java/example/db/Batch.java
@@ -0,0 +1,62 @@
+package com.muxi.java.example.db;
+
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * Mybatis Batch ( Insert / Update )
+ *
+ * @author jjl
+ * @date 2022/5/16
+ */
+@Component
+public class Batch {
+
+    @Resource
+    private SqlSessionFactory sqlSessionFactory;
+
+    /**
+     * 每次处理1000条
+     */
+    private static final int BATCH_SIZE = 1000;
+
+    /**
+     * 批量处理修改或者插入
+     *
+     * @param data        需要被处理的数据
+     * @param mapperClass Mybatis的Mapper类
+     * @param function    自定义处理逻辑
+     * @return int 影响的总行数
+     */
+    public <T, U, R> int batchInsertOrUpdate(List<T> data, Class<U> mapperClass, BiFunction<T, U, R> function) {
+        int i = 1;
+        SqlSession batchSqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
+        try {
+            U mapper = batchSqlSession.getMapper(mapperClass);
+            int size = data.size();
+            for (T element : data) {
+                function.apply(element, mapper);
+                if ((i % BATCH_SIZE == 0) || i == size) {
+                    batchSqlSession.flushStatements();
+                }
+                i++;
+            }
+            // 非事务环境下强制commit，事务情况下该commit相当于无效
+            batchSqlSession.commit(!TransactionSynchronizationManager.isSynchronizationActive());
+        } catch (Exception e) {
+            batchSqlSession.rollback();
+            e.printStackTrace();
+        } finally {
+            batchSqlSession.close();
+        }
+        return i - 1;
+    }
+
+}
