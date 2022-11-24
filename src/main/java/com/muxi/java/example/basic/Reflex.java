98bfc6839cd64eeccc6760c680c324268b16fdc2
jiangjialiang <jiangjialiang0746@163.com>
2022-11-24 11:17:50 +0800

    no message

--- src/main/java/com/muxi/java/example/basic/Reflex.java
+++ src/main/java/com/muxi/java/example/basic/Reflex.java
@@ -1,8 +1,10 @@
 package com.muxi.java.example.basic;
 
+import cn.hutool.json.JSONUtil;
 import com.muxi.java.example.domain.User;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 /**
  * reflex
@@ -11,10 +13,26 @@ import java.lang.reflect.Field;
  * @date 2022/11/11
  */
 public class Reflex {
+
+    public static <T> T clearField(T t) {
+        Class<?> clazz = t.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        Arrays.asList(fields).forEach(field -> {
+            try {
+                field.setAccessible(true);
+                field.set(t, null);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return t;
+    }
+
     public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException {
         User user = new User();
         user.setName("jack");
         user.setAge(20);
+        System.out.println(JSONUtil.toJsonStr(user));
 //        Class<?> clazz = user.getClass();
         Class<?> clazz = Class.forName("com.muxi.java.example.domain.User");
         Field[] fields = clazz.getDeclaredFields();
@@ -31,5 +49,8 @@ public class Reflex {
                 System.out.println(name + "--" + field.get(user));
             }
         }
+
+        user = clearField(user);
+        System.out.println(JSONUtil.toJsonStr(user));
     }
 }
